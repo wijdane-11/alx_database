3c19cbdb6c49040d026e4a34bcc9ad83ca31ce10
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-09 15:57:13 +0100

    upload

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
-echo "CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';" | mysql -hlocalhost -uroot -pYourMySQLRootPasswordHere
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -pYourMySQLRootPasswordHere
-echo "FLUSH PRIVILEGES;" | mysql -hlocalhost -uroot -pYourMySQLRootPasswordHere
+-- Create user user_0d_1 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1 on all databases
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
