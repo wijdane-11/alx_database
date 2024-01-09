6be699dac183a04fd218341a1b5d5d81f5445c1e
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-09 14:20:52 +0100

    upload

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,1 +1,1 @@
-sed -i '1s/^/-- Start by comments\n/' /tmp/correction/6269231728111755698091518744617555909451_5/2088/532101/SQL_more_queries/0-privileges.sql
+sed -i '1s/^/-- Start by comments\n/' /tmp/correction/5125994007846912038057979906679528902846_5/2088/532101/SQL_more_queries/0-privileges.sql
