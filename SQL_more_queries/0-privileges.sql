5c0f4de52393ef271e3c89deb5ca536999d1960b
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-09 12:21:43 +0100

    UPLOAD

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,8 +1,3 @@
-# Create user_0d_1
-echo "CREATE USER 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+echo "-- This script grants privileges to users user_0d_1 and user_0d_2" > /tmp/correction/5010036725907195453478486582812963239772_5/2088/532101/SQL_more_queries/0-privileges.sql
 
-# Create user_0d_2
-echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
-cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+cat /tmp/correction/5010036725907195453478486582812963239772_5/2088/532101/SQL_more_queries/0-privileges.sql >> /tmp/correction/5010036725907195453478486582812963239772_5/2088/532101/SQL_more_queries/0-privileges.sql
