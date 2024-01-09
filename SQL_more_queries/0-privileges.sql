ceaf512786d88d1beda603e4c6f972d41eff1592
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-09 12:48:05 +0100

    up

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,1 @@
-echo "-- This script grants privileges to users user_0d_1 and user_0d_2" > /tmp/correction/5010036725907195453478486582812963239772_5/2088/532101/SQL_more_queries/0-privileges.sql
-
-cat /tmp/correction/5010036725907195453478486582812963239772_5/2088/532101/SQL_more_queries/0-privileges.sql >> /tmp/correction/5010036725907195453478486582812963239772_5/2088/532101/SQL_more_queries/0-privileges.sql
+echo "-- This script grants privileges to users user_0d_1 and user_0d_2" | cat - /tmp/correction/5010036725907195453478486582812963239772_5/2088/532101/SQL_more_queries/0-privileges.sql > /tmp/correction/5010036725907195453478486582812963239772_5/2088/532101/SQL_more_queries/tempfile && mv /tmp/correction/5010036725907195453478486582812963239772_5/2088/532101/SQL_more_queries/tempfile /tmp/correction/5010036725907195453478486582812963239772_5/2088/532101/SQL_more_queries/0-privileges.sql
