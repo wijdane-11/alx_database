a4feb38583508198edf4042bb2b8ad2e7947e91e
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-09 12:13:29 +0100

    UPLOAD

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+# Create user_0d_1
+echo "CREATE USER 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+
+# Create user_0d_2
+echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
