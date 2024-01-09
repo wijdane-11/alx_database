8d9526d640ba83cf3f61b17c5698c1f64130badf
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-09 16:24:51 +0100

    up

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,8 @@
 USE hbtn_0d_usa;
 
+-- Select cities.id, cities.name as city_name, states.name as state_name
+-- from cities table and states table joined on state_id and id respectively
+-- Order the results by cities.id in ascending order
 SELECT cities.id, cities.name AS city_name, states.name AS state_name
 FROM cities
 JOIN states ON cities.state_id = states.id
