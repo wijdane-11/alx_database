4acc9453bd40a74347fe9d5a3e661f7475f1d518
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-09 16:19:56 +0100

    up

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,9 @@
 USE hbtn_0d_usa;
 
+-- Select the id of the state named 'California' from the states table
 SELECT * FROM cities
 WHERE state_id = (
+    -- Subquery to retrieve the id of the state named 'California'
     SELECT id FROM states
     WHERE name = 'California'
 )
